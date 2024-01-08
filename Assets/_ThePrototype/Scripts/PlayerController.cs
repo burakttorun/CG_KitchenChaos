c7101d07ade028833c8c29ffa84f7b0dfa57da7f
burakttorun <burakttorun@hotmail.com>
2024-01-08 16:48:35 +0300

    Added simple movement script to character.

---
+++ Assets/_ThePrototype/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThePrototype.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float _movementSpeed = 7f;
+
+        private void Update()
+        {
+            Vector2 inputVector = new Vector2(0, 0);
+            if (Input.GetKey(KeyCode.W))
+            {
+                inputVector.y += 1;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                inputVector.y -= 1;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                inputVector.x += 1;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                inputVector.x -= 1;
+            }
+            inputVector.Normalize();
+
+               Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+               transform.position += moveDir * (Time.deltaTime * _movementSpeed);
+        }
+    }
+}
