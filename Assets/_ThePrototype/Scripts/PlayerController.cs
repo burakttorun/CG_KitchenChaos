93c223de71f64f752d8ea6e13e544e9b68964d37
burakttorun <burakttorun@hotmail.com>
2024-01-09 14:48:30 +0300

    Collision detection added to character.

--- Assets/_ThePrototype/Scripts/PlayerController.cs
+++ Assets/_ThePrototype/Scripts/PlayerController.cs
@@ -10,13 +10,15 @@ namespace ThePrototype.Scripts
 
         #endregion
 
-        [Header("References")]
-        [SerializeField] private InputManager.InputManager _inputManager;
+        [Header("References")] [SerializeField]
+        private InputManager.InputManager _inputManager;
 
-        [Header("Settings")]
-        [SerializeField] private float _movementSpeed = 7f;
+        [Header("Settings")] [SerializeField] private float _movementSpeed = 7f;
         [SerializeField] private float _rotationSpeed = 10f;
+        [SerializeField] private float _playerRadius = 0.9f;
+        [SerializeField] private float _playerHeight = 2f;
         private bool _isWalking;
+        private float _moveDistance;
         public bool IsWalking => _isWalking;
 
         private void Start()
@@ -29,10 +31,37 @@ namespace ThePrototype.Scripts
             Vector2 inputVector = _inputManager.GetMovementVectorNormalized();
             Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
             _isWalking = moveDir != Vector3.zero;
+            _moveDistance = Time.deltaTime * _movementSpeed;
+            bool canMove = !CapsuleRayCast(moveDir);
 
-            _transform.position += moveDir * (Time.deltaTime * _movementSpeed);
+            if (!canMove)
+            {
+                Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+                canMove = !CapsuleRayCast(moveDirX);
+                if (canMove)
+                {
+                    moveDir = moveDirX;
+                }
+                else
+                {
+                    Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                    canMove = !CapsuleRayCast(moveDirZ);
+                    if (canMove)
+                    {
+                        moveDir = moveDirZ;
+                    }
+                    else return;
+                }
+            }
 
+            _transform.position += moveDir * _moveDistance;
             _transform.forward = Vector3.Slerp(_transform.forward, moveDir, Time.deltaTime * _rotationSpeed);
+        }
+
+        private bool CapsuleRayCast(Vector3 moveDir)
+        {
+            return Physics.CapsuleCast(_transform.position, _transform.position + Vector3.up * _playerHeight,
+                _playerRadius, moveDir, _moveDistance);
         }
     }
 }
