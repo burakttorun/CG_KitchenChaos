b40767de72c61c927b8fec3002bbe94e4c58b8ce
burakttorun <burakttorun@hotmail.com>
2024-01-12 17:01:09 +0300

    Empty Counter interaction behavior added.

--- Assets/ThePrototype/Scripts/EmptyCounterController.cs
+++ Assets/ThePrototype/Scripts/EmptyCounterController.cs
@@ -6,7 +6,25 @@ namespace ThePrototype.Scripts
     {
         public override void Interact(IInteractor interactor)
         {
-            throw new System.NotImplementedException();
+            if (interactor is PlayerController playerController)
+            {
+                if (!this.HasKitchenObject())
+                {
+                    if (!playerController.HasKitchenObject()) return;
+                    else
+                    {
+                        playerController.KitchenObject.ParentObject = this;
+                    }
+                }
+                else
+                {
+                    if (playerController.HasKitchenObject()) return;
+                    else
+                    {
+                        this.KitchenObject.ParentObject = playerController;
+                    }
+                }
+            }
         }
     }
 }
